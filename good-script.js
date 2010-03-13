c5677c1898e7f98209319e20bf07a24bd21121e6
passcod <none@none>
2010-03-13 21:20:24 +1300

    Efficiency: Instead of requesting the ui scripts/css each time the Controls were called, it does so once and then never again. Added: Users tab on Controls. Nothing implemented yet. Removed: 'Not implemented' messages on Controls.

--- good-script.js
+++ good-script.js
@@ -1,5 +1,8 @@
 <?php ob_start(); ?>
 function editCal() {
+
+	$('body').append('<span id="colourcal-data-store" style="width: 0px; height: 0px; display: none; visibility: hidden;"></span>');
+
 	function getColor(ele)
 	{
 		ele = $(ele);
@@ -81,79 +84,101 @@ function submitCal()
 
 function showControls()
 {	
-	$.getScript(page_self+'?data=uijs', function() {
-		$.get(page_self+'?data=uicss', function(css) {
-			$('head').append('<style type="text/css">'+css+"\n\n"+
-			"#controls-colours-list { list-style-type: none; margin: 0; padding: 0; }\n"+
-			"#controls-colours-list li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 0.8em; height: 15px; }\n"+
-			"#controls-buttons { float: right; }\n"+
-			"#controls-buttons button * { font-size: 1em; height: 1.1em; margin: 0; padding: 0.2em; }\n"+
-			'</style>');
-			$('body').append('<div id="controls" style="display: none;"></div>');
-			
-			var tabs_str = ""+
-			"<div id='tabs'>\n"+
-			"	<ul>\n"+
-			"		<li><a href='#tabs-1'>Colours</a></li>\n"+
-			"		<li><a href='#tabs-2'>About</a></li>\n"+
-			"	</ul>\n"+
-			"	<div id='tabs-1'>\n"+
-			"		<p>Available colours:</p>\n"+
-			"		<ul id='controls-colours-list'>\n"+
-			"		</ul>\n"+
-			"		<button id='controls-colours-add'>Add a colour</button>\n"+
-			"	</div>\n"+
-			"	<div id='tabs-2'>\n"+
-			"		<p>ColourCal is a project by: passcod</p>\n"+
-			"		<p><a href='http://colourcal.sourceforge.net'>http://colourcal.sourceforge.net</a></p>\n"+
-			"	</div>\n"+
-			"<div id='controls-buttons'>\n"+
-			"	<button id='controls-save'>Save</button>\n"+
-			"	<button id='controls-cancel'>Cancel</button>\n"+
-			"</div>\n"+
-			"</div>";
-			
-			var ctrl = $('#controls');
-			ctrl.html(tabs_str);
-			
-			var tabs = ctrl.find('#tabs');
-			tabs.tabs();
-			
-			var colours_list = ctrl.find('#controls-colours-list');
-			
-			for( i in colors )
-			{
-				colours_list.append('<li class="ui-state-default">'+colors[i]+' - #'+color_codes[i]+' - <span style="background-color: #'+color_codes[i]+'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span style="float: right"><a class="controls-colours-edit">Edit</a> <a class="controls-colours-remove">Remove</a></span></li>'+"\n");
-			}
-			
-			colours_list.sortable();
-			colours_list.disableSelection();
-			
-			ctrl.find('#controls-colours-add').button().click(function() {
-				ctrl.append('<div id="controls-ni" style="display: none;">Not Implemented (yet).</div>');
-				ctrl.find('#controls-ni').dialog();
-			});
-			ctrl.find('.controls-colours-edit').click(function() {
-				ctrl.append('<div id="controls-ni" style="display: none;">Not Implemented (yet).</div>');
-				ctrl.find('#controls-ni').dialog();
-			});
-			ctrl.find('.controls-colours-remove').click(function() {
-				ctrl.append('<div id="controls-ni" style="display: none;">Not Implemented (yet).</div>');
-				ctrl.find('#controls-ni').dialog();
-			});
-			
-			ctrl.find('#controls-save').button().click(function() {
-				ctrl.append('<div id="controls-ni" style="display: none;">Not Implemented (yet).</div>');
-				ctrl.find('#controls-ni').dialog();
-			});
-			ctrl.find('#controls-cancel').button().click(function() {
-				ctrl.dialog('close');
-			});
-			
-			ctrl.dialog({ modal: true, title: "Controls", width: 600, maxHeight: 500 });
-			
+	var showControlsPopup = function() {
+		$('body').append('<div id="controls" style="display: none;"></div>');
+		
+		var tabs_str = ""+
+		"<div id='tabs'>\n"+
+		"	<ul>\n"+
+		"		<li><a href='#tabs-1'>Colours</a></li>\n"+
+		"		<li><a href='#tabs-2'>Users</a></li>\n"+
+		"		<li><a href='#tabs-3'>About</a></li>\n"+
+		"	</ul>\n"+
+		"	<div id='tabs-1'>\n"+
+		"		<p>Available colours:</p>\n"+
+		"		<ul id='controls-colours-list'>\n"+
+		"		</ul>\n"+
+		"		<button id='controls-colours-add'>Add a colour</button>\n"+
+		"	</div>\n"+
+		"	<div id='tabs-2'>\n"+
+		"		<p>This is not implemented yet</p>\n"+
+		"		<p>Feature Coming Soon</p>\n"+
+		"	</div>\n"+
+		"	<div id='tabs-3'>\n"+
+		"		<p>ColourCal is a project by: passcod</p>\n"+
+		"		<p><a href='http://colourcal.sourceforge.net'>http://colourcal.sourceforge.net</a></p>\n"+
+		"	</div>\n"+
+		"<div id='controls-buttons'>\n"+
+		"	<button id='controls-save'>Save</button>\n"+
+		"	<button id='controls-cancel'>Cancel</button>\n"+
+		"</div>\n"+
+		"</div>";
+		
+		var ctrl = $('#controls');
+		ctrl.html(tabs_str);
+		
+		var tabs = ctrl.find('#tabs');
+		tabs.tabs();
+		
+		var colours_list = ctrl.find('#controls-colours-list');
+		
+		for( i in colors )
+		{
+			colours_list.append('<li class="ui-state-default">'+colors[i]+' - #'+color_codes[i]+' - <span style="background-color: #'+color_codes[i]+'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span style="float: right"><a class="controls-colours-edit">Edit</a> <a class="controls-colours-remove">Remove</a></span></li>'+"\n");
+		}
+		
+		colours_list.sortable();
+		colours_list.disableSelection();
+		
+		ctrl.find('#controls-colours-add').button().click(coloursAdd);
+		ctrl.find('.controls-colours-edit').click(coloursEdit);
+		ctrl.find('.controls-colours-remove').click(coloursRemove);
+		
+		ctrl.find('#controls-save').button().click(coloursSave);
+		ctrl.find('#controls-cancel').button().click(function() {
+			ctrl.dialog('close');
 		});
-	});
+		
+		ctrl.dialog({ modal: true, title: "Controls", width: 600, maxHeight: 500 });
+		
+		var coloursAdd = function() {
+			//
+		};
+		
+		var coloursEdit = function() {
+			//
+		};
+		
+		var coloursRemove = function() {
+			//
+		};
+		
+		var coloursSave = function() {
+			//
+		};
+	};
+	
+	
+	if ( $.data($('#colourcal-data-store').get(0), "controls-state") == 'loaded' )
+	{
+		showControlsPopup();
+	}
+	else
+	{
+		$.getScript(page_self+'?data=uijs', function() {
+			$.get(page_self+'?data=uicss', function(css) {
+				$('head').append('<style type="text/css">'+css+"\n\n"+
+				"#controls-colours-list { list-style-type: none; margin: 0; padding: 0; }\n"+
+				"#controls-colours-list li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 0.8em; height: 15px; }\n"+
+				"#controls-buttons { float: right; }\n"+
+				"#controls-buttons button * { font-size: 1em; height: 1.1em; margin: 0; padding: 0.2em; }\n"+
+				'</style>');
+			
+				$.data($('#colourcal-data-store').get(0), "controls-state", 'loaded');
+				showControlsPopup();
+			});
+		});
+	}
 }
 
 <?php $good_script = ob_get_clean(); ?>
