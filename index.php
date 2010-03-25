f2529c7f00d1aec2449dba147c9d041ce2e221bf
passcod <none@none>
2010-03-25 18:40:49 +1300

    ...
    --HG--
    branch : adobe-air

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/** Runfile for ColourCal.
+ * Run this file to test the app.
+ */
+
+$version_short = '0.2';
+$version = '1-'.$version_short.'-'.date('y.W.N-H.i.s');
+ob_start();
+?>
+
+<?php include('jquery.js'); ?>
+<?php include('ui.jquery.js'); ?>
+<?php include('ui.jquery.css'); ?>
+<?php include('colorpicker.jquery.js'); ?>
+<?php include('colorpicker.jquery.css'); ?>
+<?php include('md5.jquery.js'); ?>
+<?php include('good-script.js'); ?>
+<?php include('bad-script.js'); ?>
+<?php include('data-control.php'); ?>
+<?php include('page.php'); ?>
+
+<?php ob_end_flush(); ?>
