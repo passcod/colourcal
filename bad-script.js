a68f6fed5acfdfb6dc53b3ed778c561113f082a7
passcod <none@none>
2010-03-25 18:33:26 +1300

    Jetpack version init
    --HG--
    branch : jetpack

--- bad-script.js
+++
@@ -1,8 +0,0 @@
-<?php ob_start();
-?>
-function editCal()
-{
-	$('#login_form').children().remove();
-	$('#login_form').prepend('<p style=\"font-weight: bold; background-color: red;\">Wrong password - <a onclick=\"location.reload(true);\">try again</a></p>');
-}
-<?php $bad_script = ob_get_clean(); ?>
